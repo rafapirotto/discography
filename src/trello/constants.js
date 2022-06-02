00b7952e79657e5ede8ce4b7c8b44ef7f563e4b4
rafapirotto <33788807+rafapirotto@users.noreply.github.com>
2022-06-02 19:14:13 -0300

    Add create trello board feature (#2)

---
+++ src/trello/constants.js
@@ -0,0 +1,7 @@
+const BOARD_NAME = "Bob Dylan's Discography";
+const BOARD_DESCRIPTION = "This board shows all of Bob Dylan's Discography";
+
+module.exports = {
+  BOARD_NAME,
+  BOARD_DESCRIPTION,
+};
---
+++ src/trello/index.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+
+const { BOARD_NAME, BOARD_DESCRIPTION } = require('./constants');
+
+const { TRELLO_API_KEY, TRELLO_TOKEN } = process.env;
+
+const CREATE_BOARD_URL = `https://api.trello.com/1/boards/?name=${BOARD_NAME}&key=${TRELLO_API_KEY}&token=${TRELLO_TOKEN}`;
+
+const createBoard = async () => {
+  try {
+    const requestParams = { defaultLists: false, desc: BOARD_DESCRIPTION };
+    const { data } = await axios.post(CREATE_BOARD_URL, requestParams);
+
+    return data?.id;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { createBoard };
